15154ec53ff34e44479481d53687498850fbfebd
Valentín Yedro <vnyedro@gmail.com>
2021-08-30 19:35:06 -0300

    Comienzo del TP1

---
+++ ArbolGeneral.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace tp1
+{
+	public class ArbolGeneral<T>
+	{
+		
+		private T dato;
+		private List<ArbolGeneral<T>> hijos = new List<ArbolGeneral<T>>();
+
+		public ArbolGeneral(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public List<ArbolGeneral<T>> getHijos() {
+			return hijos;
+		}
+	
+		public void agregarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Add(hijo);
+		}
+	
+		public void eliminarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Remove(hijo);
+		}
+	
+		public bool esHoja() {
+			return this.getHijos().Count == 0;
+		}
+	
+		public int altura() {
+			return 0;
+		}
+	
+		
+		public int nivel(T dato) {
+			return 0;
+		}
+	
+	}
+}
